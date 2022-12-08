b3eaec5058f6126f62a7ce450b5c639649aa7021
Rapcsak13 <78954622+Rapcsak13@users.noreply.github.com>
2022-12-08 13:43:33 +0100

    Add files via upload

---
+++ rust2/rust2/Form1.Designer.cs
@@ -0,0 +1,331 @@
+﻿
+namespace rust2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.pont = new System.Windows.Forms.Button();
+            this.upgrade1 = new System.Windows.Forms.Button();
+            this.upgrade2 = new System.Windows.Forms.Button();
+            this.upgrade3 = new System.Windows.Forms.Button();
+            this.upgrade4 = new System.Windows.Forms.Button();
+            this.upgrade5 = new System.Windows.Forms.Button();
+            this.upgrade6 = new System.Windows.Forms.Button();
+            this.wb = new System.Windows.Forms.Button();
+            this.számláló = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.timer3 = new System.Windows.Forms.Timer(this.components);
+            this.timer4 = new System.Windows.Forms.Timer(this.components);
+            this.timer5 = new System.Windows.Forms.Timer(this.components);
+            this.timer6 = new System.Windows.Forms.Timer(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // pont
+            // 
+            this.pont.BackColor = System.Drawing.SystemColors.Control;
+            this.pont.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pont.BackgroundImage")));
+            this.pont.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.pont.Location = new System.Drawing.Point(156, 0);
+            this.pont.Margin = new System.Windows.Forms.Padding(4);
+            this.pont.Name = "pont";
+            this.pont.Size = new System.Drawing.Size(756, 416);
+            this.pont.TabIndex = 0;
+            this.pont.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.pont.UseVisualStyleBackColor = false;
+            this.pont.Click += new System.EventHandler(this.pont_Click);
+            // 
+            // upgrade1
+            // 
+            this.upgrade1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("upgrade1.BackgroundImage")));
+            this.upgrade1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.upgrade1.Location = new System.Drawing.Point(3, 0);
+            this.upgrade1.Margin = new System.Windows.Forms.Padding(4);
+            this.upgrade1.Name = "upgrade1";
+            this.upgrade1.Size = new System.Drawing.Size(145, 97);
+            this.upgrade1.TabIndex = 1;
+            this.upgrade1.UseVisualStyleBackColor = true;
+            this.upgrade1.Click += new System.EventHandler(this.upgrade1_Click);
+            // 
+            // upgrade2
+            // 
+            this.upgrade2.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("upgrade2.BackgroundImage")));
+            this.upgrade2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.upgrade2.Location = new System.Drawing.Point(3, 160);
+            this.upgrade2.Margin = new System.Windows.Forms.Padding(4);
+            this.upgrade2.Name = "upgrade2";
+            this.upgrade2.Size = new System.Drawing.Size(145, 97);
+            this.upgrade2.TabIndex = 2;
+            this.upgrade2.UseVisualStyleBackColor = true;
+            this.upgrade2.Click += new System.EventHandler(this.upgrade2_Click);
+            // 
+            // upgrade3
+            // 
+            this.upgrade3.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("upgrade3.BackgroundImage")));
+            this.upgrade3.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.upgrade3.Location = new System.Drawing.Point(3, 319);
+            this.upgrade3.Margin = new System.Windows.Forms.Padding(4);
+            this.upgrade3.Name = "upgrade3";
+            this.upgrade3.Size = new System.Drawing.Size(145, 97);
+            this.upgrade3.TabIndex = 3;
+            this.upgrade3.UseVisualStyleBackColor = true;
+            this.upgrade3.Click += new System.EventHandler(this.upgrade3_Click);
+            // 
+            // upgrade4
+            // 
+            this.upgrade4.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("upgrade4.BackgroundImage")));
+            this.upgrade4.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.upgrade4.Location = new System.Drawing.Point(920, 0);
+            this.upgrade4.Margin = new System.Windows.Forms.Padding(4);
+            this.upgrade4.Name = "upgrade4";
+            this.upgrade4.Size = new System.Drawing.Size(145, 97);
+            this.upgrade4.TabIndex = 4;
+            this.upgrade4.UseVisualStyleBackColor = true;
+            this.upgrade4.Click += new System.EventHandler(this.upgrade4_Click);
+            // 
+            // upgrade5
+            // 
+            this.upgrade5.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("upgrade5.BackgroundImage")));
+            this.upgrade5.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.upgrade5.Location = new System.Drawing.Point(920, 160);
+            this.upgrade5.Margin = new System.Windows.Forms.Padding(4);
+            this.upgrade5.Name = "upgrade5";
+            this.upgrade5.Size = new System.Drawing.Size(145, 97);
+            this.upgrade5.TabIndex = 5;
+            this.upgrade5.UseVisualStyleBackColor = true;
+            this.upgrade5.Click += new System.EventHandler(this.upgrade5_Click);
+            // 
+            // upgrade6
+            // 
+            this.upgrade6.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("upgrade6.BackgroundImage")));
+            this.upgrade6.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.upgrade6.Location = new System.Drawing.Point(920, 319);
+            this.upgrade6.Margin = new System.Windows.Forms.Padding(4);
+            this.upgrade6.Name = "upgrade6";
+            this.upgrade6.Size = new System.Drawing.Size(145, 97);
+            this.upgrade6.TabIndex = 6;
+            this.upgrade6.UseVisualStyleBackColor = true;
+            this.upgrade6.Click += new System.EventHandler(this.upgrade6_Click);
+            // 
+            // wb
+            // 
+            this.wb.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("wb.BackgroundImage")));
+            this.wb.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.wb.Location = new System.Drawing.Point(211, 417);
+            this.wb.Margin = new System.Windows.Forms.Padding(4);
+            this.wb.Name = "wb";
+            this.wb.Size = new System.Drawing.Size(207, 132);
+            this.wb.TabIndex = 7;
+            this.wb.UseVisualStyleBackColor = true;
+            this.wb.Click += new System.EventHandler(this.wb_Click);
+            // 
+            // számláló
+            // 
+            this.számláló.AutoSize = true;
+            this.számláló.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.számláló.Location = new System.Drawing.Point(688, 469);
+            this.számláló.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.számláló.Name = "számláló";
+            this.számláló.Size = new System.Drawing.Size(23, 25);
+            this.számláló.TabIndex = 8;
+            this.számláló.Text = "0";
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 400;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // timer2
+            // 
+            this.timer2.Interval = 300;
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
+            // 
+            // timer3
+            // 
+            this.timer3.Interval = 250;
+            this.timer3.Tick += new System.EventHandler(this.timer3_Tick);
+            // 
+            // timer4
+            // 
+            this.timer4.Interval = 75;
+            this.timer4.Tick += new System.EventHandler(this.timer4_Tick);
+            // 
+            // timer5
+            // 
+            this.timer5.Interval = 25;
+            this.timer5.Tick += new System.EventHandler(this.timer5_Tick);
+            // 
+            // timer6
+            // 
+            this.timer6.Interval = 1;
+            this.timer6.Tick += new System.EventHandler(this.timer6_Tick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label1.Location = new System.Drawing.Point(425, 469);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(169, 25);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "1 NEXT: 300 pont";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label2.Location = new System.Drawing.Point(29, 118);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(77, 25);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "50 pont";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label3.Location = new System.Drawing.Point(29, 278);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(88, 25);
+            this.label3.TabIndex = 11;
+            this.label3.Text = "100 pont";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label4.Location = new System.Drawing.Point(29, 438);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(88, 25);
+            this.label4.TabIndex = 12;
+            this.label4.Text = "500 pont";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label5.Location = new System.Drawing.Point(941, 118);
+            this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(99, 25);
+            this.label5.TabIndex = 13;
+            this.label5.Text = "1000 pont";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label6.Location = new System.Drawing.Point(941, 278);
+            this.label6.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(99, 25);
+            this.label6.TabIndex = 14;
+            this.label6.Text = "2000 pont";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label7.Location = new System.Drawing.Point(941, 438);
+            this.label7.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(99, 25);
+            this.label7.TabIndex = 15;
+            this.label7.Text = "5000 pont";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1067, 554);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.számláló);
+            this.Controls.Add(this.wb);
+            this.Controls.Add(this.upgrade6);
+            this.Controls.Add(this.upgrade5);
+            this.Controls.Add(this.upgrade4);
+            this.Controls.Add(this.upgrade3);
+            this.Controls.Add(this.upgrade2);
+            this.Controls.Add(this.upgrade1);
+            this.Controls.Add(this.pont);
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button pont;
+        private System.Windows.Forms.Button upgrade1;
+        private System.Windows.Forms.Button upgrade2;
+        private System.Windows.Forms.Button upgrade3;
+        private System.Windows.Forms.Button upgrade4;
+        private System.Windows.Forms.Button upgrade5;
+        private System.Windows.Forms.Button upgrade6;
+        private System.Windows.Forms.Button wb;
+        private System.Windows.Forms.Label számláló;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Timer timer2;
+        private System.Windows.Forms.Timer timer3;
+        private System.Windows.Forms.Timer timer4;
+        private System.Windows.Forms.Timer timer5;
+        private System.Windows.Forms.Timer timer6;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+    }
+}
+
---
+++ rust2/rust2/Form1.cs
@@ -0,0 +1,288 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace rust2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        int i =1;
+        int wblv = 1;
+        int szorzo = 1;
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+        int counter2 = 0;
+        private void pont_Click(object sender, EventArgs e)
+        {
+            if (szorzo == 1)
+            {
+                számláló.Text = i + "";
+                i++;
+            }
+            if (szorzo == 2)
+            {
+                for (counter2 = 1; counter2 <= 2; counter2++)
+                {
+                    számláló.Text = i + "";
+                    i++;
+                }
+            }
+            if (szorzo == 3)
+            {
+                for (counter2 = 1; counter2 <= 4; counter2++)
+                {
+                    számláló.Text = i + "";
+                    i++;
+                }
+            }
+            if (szorzo == 4)
+            {
+                for (counter2 = 1; counter2 <= 8; counter2++)
+                {
+                    számláló.Text = i + "";
+                    i++;
+                }
+            }
+            if (szorzo == 5)
+            {
+                for (counter2 = 1; counter2 <= 16; counter2++)
+                {
+                    számláló.Text = i + "";
+                    i++;
+                }
+            }
+            if (szorzo == 6)
+            {
+                for (counter2 = 1; counter2 <= 32; counter2++)
+                {
+                    számláló.Text = i + "";
+                    i++;
+                }
+            }
+            if (szorzo == 7)
+            {
+                for (counter2 = 1; counter2 <= 100; counter2++)
+                {
+                    számláló.Text = i + "";
+                    i++;
+                }
+            }
+
+            if (i > 20 && szorzo == 1)
+            { 
+                    pont.BackgroundImage = rust2.Properties.Resources.rust_spear_removebg_preview;
+                    szorzo++;
+            }
+            if (i > 100 && szorzo == 2)
+            {
+                pont.BackgroundImage = rust2.Properties.Resources.rust_crossbow;
+                szorzo++;
+            }
+            if (i > 300 && szorzo == 3 && wblv == 2)
+            {
+                pont.BackgroundImage = rust2.Properties.Resources.rust_semi_pistol;
+                szorzo++;   
+            }
+            if (i > 500 && szorzo == 4 && wblv == 2)
+            {
+                pont.BackgroundImage = rust2.Properties.Resources.rust_semi;
+                szorzo++;
+            }
+            if (i > 2000 && szorzo == 5 && wblv == 3)
+            {
+                pont.BackgroundImage = rust2.Properties.Resources.rust_lr;
+                szorzo++;
+            }
+            if (i > 3000 && szorzo == 6 && wblv == 3)
+            {
+                pont.BackgroundImage = rust2.Properties.Resources.rust_ak;
+                szorzo++;
+            }
+
+
+
+        }
+        
+        int counter = 0;
+        private void upgrade1_Click(object sender, EventArgs e)
+        {
+            while (timer1.Enabled == false)
+            {
+                if(i > 50)
+                {
+                    timer1.Enabled = Enabled;
+                    for (counter = 1; counter <= 50;)
+                    {
+                        counter++;
+                        i--;
+                    }
+                    label2.Text = "MAX";
+                }
+            }
+            
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+
+           számláló.Text = i + "";
+           i++;
+         
+        }
+
+        private void upgrade2_Click(object sender, EventArgs e)
+        {
+            while (timer2.Enabled == false)
+            {
+                if (i > 100)
+                {
+                    timer2.Enabled = Enabled;
+                    for (counter = 1; counter <= 100;)
+                    {
+                        counter++;
+                        i--;
+                    }
+                    label3.Text = "MAX";
+                }
+            }
+        }
+
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+            számláló.Text = i + "";
+            i++;
+        }
+
+        private void upgrade3_Click(object sender, EventArgs e)
+        {
+            while (timer3.Enabled == false)
+            {
+                if (i > 500)
+                {
+                    timer3.Enabled = Enabled;
+                    for (counter = 1; counter <= 500;)
+                    {
+                        counter++;
+                        i--;
+                    }
+                    label4.Text = "MAX";
+                }
+            }
+        }
+
+        private void timer3_Tick(object sender, EventArgs e)
+        {
+            számláló.Text = i + "";
+            i++;
+        }
+
+        private void timer4_Tick(object sender, EventArgs e)
+        {
+            számláló.Text = i + "";
+            i++;
+        }
+
+        private void timer5_Tick(object sender, EventArgs e)
+        {
+            számláló.Text = i + "";
+            i++;
+        }
+
+        private void timer6_Tick(object sender, EventArgs e)
+        {
+            számláló.Text = i + "";
+            i++;
+        }
+
+        private void upgrade4_Click(object sender, EventArgs e)
+        {
+            while (timer4.Enabled == false)
+            {
+                if (i > 1000)
+                {
+                    timer4.Enabled = Enabled;
+                    for (counter = 1; counter <= 1000;)
+                    {
+                        counter++;
+                        i--;
+                    }
+                    label5.Text = "MAX";
+                }
+            }
+        }
+
+        private void upgrade5_Click(object sender, EventArgs e)
+        {
+            while (timer5.Enabled == false)
+            {
+                if (i > 2000)
+                {
+                    timer5.Enabled = Enabled;
+                    for (counter = 1; counter <= 2000;)
+                    {
+                        counter++;
+                        i--;
+                    }
+                    label6.Text = "MAX";
+                }
+            }
+        }
+
+        private void upgrade6_Click(object sender, EventArgs e)
+        {
+            while (timer6.Enabled == false)
+            {
+                if (i > 5000)
+                {
+                    timer6.Enabled = Enabled;   
+                    for (counter = 1; counter <= 5000;)
+                    {
+                        counter++;
+                        i--;
+                    }
+                    label7.Text = "MAX";
+                }
+            }
+        }
+
+        private void wb_Click(object sender, EventArgs e)
+        {
+            if (i > 300)
+            {
+                wb.BackgroundImage = rust2.Properties.Resources.rust_wb2;
+                if (wblv < 2)
+                {
+                    wblv++;
+                    label1.Text = wblv + "" + " NEXT: 1000 pont";
+                }
+                
+            }
+            if (i > 1000)
+            {
+                wb.BackgroundImage = rust2.Properties.Resources.rust_wb3_removebg_preview;
+                if (wblv < 3)
+                {
+                    wblv++;
+                    label1.Text = "MAX";
+                }
+            }
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
